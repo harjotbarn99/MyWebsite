fccd0e12c610d814cb8d580e665253aff7a50a68
Harjot Barn <harjotba@buffalo.edu>
2020-12-24 22:01:36 -0500

    somewhat ready for deploy

--- mainWebsite/settings/prod.py
+++ mainWebsite/settings/prod.py
@@ -1,22 +1,26 @@
 from .base import *
 import os
+import django_heroku
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["harjot-barn.herokuapp.com"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-SECRET_KEY = os.environ("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # database
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ("PDB_NAME"),
-        "USER": os.environ("PDB_USER"),
-        "PASSWORD": os.environ("PDB_PASS"),
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.environ("PDB_NAME"),
+#         "USER": os.environ("PDB_USER"),
+#         "PASSWORD": os.environ("PDB_PASS"),
+#         "HOST": "localhost",
+#         "PORT": "",
+#     }
+# }
+
+django_heroku.settings(locals())
