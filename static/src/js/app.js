46b5dc4a45070f073e2bd4da60f1a6e0584aed80
Harjot Barn <harjotba@buffalo.edu>
2020-12-27 17:29:23 -0500

    added base template

---
+++ static/src/js/app.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+// import Foundation from 'foundation-sites';
+import '../lib/foundation-explicit-pieces';
+
+$(document).foundation();
