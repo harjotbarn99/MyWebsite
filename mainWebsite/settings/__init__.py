78938c55857c1eedbceab442a6894c3779d250a2
Harjot Barn <harjotba@buffalo.edu>
2020-12-27 11:09:29 -0500

    init

--- mainWebsite/settings/__init__.py
+++ mainWebsite/settings/__init__.py
@@ -1,8 +1,17 @@
 import os
-if (os.environ.get("PRODUCTION") == "True"):
-    print("this is production------------------------------------------ ")
+try:
+    print("\n****")
+    print(os.environ.get("PRODUCTION"))
+    print("****\n")
+except:
+    print("\n****")
+    print("failed os var")
+    print("****\n")
+
+if (os.environ.get("PRODUCTION") == "False"):
+    print("this is development------------------------------------------ ")
     from .prod import *
 else:
-    print("this is development------------------------------------------- ")
+    print("this is production------------------------------------------- ")
     from .prod import *
 # from .prod import *
