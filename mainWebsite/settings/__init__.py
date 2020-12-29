7cffc98706452c99de3b57a4a0481623098f0fc8
Harjot Barn <harjotba@buffalo.edu>
2020-12-29 12:59:05 -0500

    removed debug statements

--- mainWebsite/settings/__init__.py
+++ mainWebsite/settings/__init__.py
@@ -1,13 +1,4 @@
 import os
-try:
-    print("\n****")
-    print(os.environ.get("PRODUCTION"))
-    print("****\n")
-except:
-    print("\n****")
-    print("failed os var")
-    print("****\n")
-
 if (os.environ.get("PRODUCTION") == "False"):
     print("this is development------------------------------------------ ")
     from .dev import *
