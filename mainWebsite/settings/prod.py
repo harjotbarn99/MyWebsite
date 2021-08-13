9c61fbd3ac11fed43f2f7a66c2c1ec9f98510730
Harjot Barn <harjotba@buffalo.edu>
2021-08-13 14:23:05 -0400

    storage changed to dropbox

--- mainWebsite/settings/prod.py
+++ mainWebsite/settings/prod.py
@@ -23,13 +23,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #     }
 # }
 
-AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
+# AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DROPBOX_OAUTH2_TOKEN=os.environ.get("DROPBOX_OAUTH2_TOKEN")
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 
 django_heroku.settings(locals())
