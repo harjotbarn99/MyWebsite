c8b833c74caacfc553d856e1a3b4e9a626eb2713
Harjot Barn <harjotba@buffalo.edu>
2023-01-02 23:34:51 -0500

    cleaned up

--- mainWebsite/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-if (os.environ.get("PRODUCTION") == "False"):
-    print("this is development------------------------------------------ ")
-    from .dev import *
-else:
-    print("this is production------------------------------------------- ")
-    from .prod import *
