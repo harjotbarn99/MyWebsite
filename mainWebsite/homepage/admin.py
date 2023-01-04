f529f1fb668cd4d6a868b7743a2be453a606ec9e
Harjot Barn <harjotba@buffalo.edu>
2023-01-03 22:07:11 -0500

    bug fix : fontawsome , models in admin panel

--- mainWebsite/homepage/admin.py
+++ mainWebsite/homepage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Work, MyIntro, SocialWebsite, HomePage, Experience
 
 # Register your models here.
+admin.site.register(Work)
+admin.site.register(MyIntro)
+admin.site.register(SocialWebsite)
+admin.site.register(HomePage)
+admin.site.register(Experience)
